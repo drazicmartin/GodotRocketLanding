1de59ad1c42e07c18398177a958fb75a29aaac75
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-10-31 00:41:44 +0100

    feat: reformat python code with an utils module

--- python/simple_landing.py
+++ python/simple_landing.py
@@ -1,68 +1,27 @@
 import asyncio
-import websockets
-import json
-import time
+from utils import GRL
 
-class GRL:
-    def __init__(self, uri):
-        self.uri = uri
-        self.websocket = None  # Initialize websocket as None
+class SimpleLanding(GRL):
+    def process(state: dict):
+        """
+        Processes the current state of the rocket.
 
-    async def connect(self):
-        # Perform the async connection setup in a separate async method
-        print(f"Connection to {self.uri}")
-        self.websocket = await websockets.connect(self.uri)
-        print("Connection succeed")
+        :param state: A dictionary representing the current state of the rocket.
+        :return: A new dictionary that specifies the input for the rocket.
+        """
 
-    async def send_data(self, data):
-        if self.websocket is not None:
-            json_data = json.dumps(data)
-            # Send JSON data to the server
-            await self.websocket.send(json_data)
-    
-    async def receive_data(self):
-        response = await self.websocket.recv()
-        # Deserialize the JSON string back into a Python object
-        response_data = json.loads(response)
-        return response_data
+        inputs = {
+            "main_thrust": 0.4,
+            "rcs_left_thrust": 0,
+            "rcs_right_thrust": 0
+        }
 
-    async def crash(self):
-        if self.websocket is not None:
-            await self.websocket.close()
+        print(state)
 
-    async def ignition(self):
-        await self.connect()
-        while True:
-            state = await self.receive_data()
-
-            if "game_state" in state:
-                print(state['game_state'])
-                break
-
-            inputs = process(state)
-            await self.send_data(inputs)
-        await self.crash()
-
-def process(state: dict):
-    """
-    Processes the current state of the rocket.
-
-    :param state: A dictionary representing the current state of the rocket.
-    :return: A new dictionary that specifies the input for the rocket.
-    """
-
-    inputs = {
-        "main_thrust": 0.4,
-        "rcs_left_thrust": 0,
-        "rcs_right_thrust": 0
-    }
-
-    print(state)
-
-    return inputs
+        return inputs
 
 if __name__ == "__main__":
-    rocket = GRL(
+    rocket = SimpleLanding(
         uri="ws://127.0.0.1:65000"
     ) 
     try:
---
+++ python/utils.py
@@ -0,0 +1,47 @@
+import websockets
+import json
+from abc import ABC, abstractmethod
+
+class GRL:
+    def __init__(self, uri):
+        self.uri = uri
+        self.websocket = None  # Initialize websocket as None
+
+    async def connect(self):
+        # Perform the async connection setup in a separate async method
+        print(f"Connection to {self.uri}")
+        self.websocket = await websockets.connect(self.uri)
+        print("Connection succeed")
+
+    async def send_data(self, data):
+        if self.websocket is not None:
+            json_data = json.dumps(data)
+            # Send JSON data to the server
+            await self.websocket.send(json_data)
+    
+    async def receive_data(self):
+        response = await self.websocket.recv()
+        # Deserialize the JSON string back into a Python object
+        response_data = json.loads(response)
+        return response_data
+
+    async def crash(self):
+        if self.websocket is not None:
+            await self.websocket.close()
+
+    async def ignition(self):
+        await self.connect()
+        while True:
+            state = await self.receive_data()
+
+            if "game_state" in state:
+                print(state['game_state'])
+                break
+
+            inputs = self.process(state)
+            await self.send_data(inputs)
+        await self.crash()
+
+    @abstractmethod
+    def process(self, state: dict):
+        pass
