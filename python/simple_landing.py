f031719df8a4d2358c1f2b350eaddd080e5fc8fb
MARTIN Drazic <drazic9@live.fr>
2025-04-08 17:14:23 +0200

    feat: reset simple landing process and port

--- python/simple_landing.py
+++ python/simple_landing.py
@@ -9,27 +9,15 @@ class SimpleLanding(GRL):
         :param state: A dictionary representing the current state of the rocket.
         :return: A new dictionary that specifies the input for the rocket.
         """
-        position = state['position']
-        velocity = state['linear_velocity']
 
+        main_thrust = 0
         rcs_left_thrust = 0
         rcs_right_thrust = 0
 
-        if state['rotation'] > 0:
-            rcs_right_thrust = 0.5
-        else:
-            rcs_left_thrust = 0.5
-
-        t = 0.1
-        if position[1] > -200:
-            t = 0.5
-        if position[1] > -10:
-            t = 0
-
         action = {
-            "main_thrust": 0,
-            "rcs_left_thrust": 0,
-            "rcs_right_thrust": 0,
+            "main_thrust": main_thrust,
+            "rcs_left_thrust": rcs_left_thrust,
+            "rcs_right_thrust": rcs_right_thrust,
         }
 
         return action
@@ -42,7 +30,7 @@ async def main(rocket: GRL, level_name='random_level_easy', show_window=False):
 
 if __name__ == "__main__":
     rocket = SimpleLanding(
-        port=3012
+        port=65000
     )
     asyncio.run(
         main(rocket, show_window=True),
